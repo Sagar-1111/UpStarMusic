490ab8323acbe2237a160f65d80378d0ba973283
sagar <sagar.raval@raweng.com>
2017-09-13 23:37:57 +0530

    Created album schema

--- database/models/album.js
+++ database/models/album.js
@@ -1,1 +1,14 @@
 // Todo: create Album Schema
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const AlbumSchema = new Schema({
+  title: String,
+  date: Date,
+  copiesSold: Number,
+  image: String,
+  numberTracks: Number,
+  revenue : Number
+});
+
+module.exports = AlbumSchema;
