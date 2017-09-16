d411837109f44b1c0736795c8c4ad5fae2c97901
sagar <sagar.raval@raweng.com>
2017-09-16 22:12:49 +0530

    search artist query completed

--- database/queries/SearchArtists.js
+++ database/queries/SearchArtists.js
@@ -13,7 +13,7 @@ module.exports = (criteria, sortProperty, offset = 0, limit = 20) => {
   // const sortOrder = {};
   // sortOrder[sortProperty] : 1;
 
-  const query = Artist.find({})
+  const query = Artist.find(buildQuery(criteria))
     .sort({ [sortProperty] :1 })
     .skip(offset)
     .limit(limit)
@@ -27,4 +27,27 @@ module.exports = (criteria, sortProperty, offset = 0, limit = 20) => {
               limit: limit
           };
       });
+};
+const buildQuery = (criteria) => {
+  const query = {};
+
+ if(criteria.name){
+   query.$text = { $search: criteria.name };
+ }
+
+  if(criteria.age){
+    query.age = {
+      $gte: criteria.age.min,
+      $lte: criteria.age.max
+    }
+  }
+
+  if(criteria.yearsActive){
+    query.yearsActive = {
+      $gte: criteria.yearsActive.min,
+      $lte: criteria.yearsActive.max
+    }
+  }
+
+  return query;
 };
